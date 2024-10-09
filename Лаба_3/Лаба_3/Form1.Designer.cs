7c2b2e2985e70639fb486459f19e0d84547be96c
piper cat <126021452+gogologs@users.noreply.github.com>
2024-10-09 17:18:25 +0300

    Add files via upload

---
+++ Лаба_3/Лаба_3/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Лаба_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.itog = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.eng = new System.Windows.Forms.CheckBox();
+            this.rus = new System.Windows.Forms.CheckBox();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.crypt = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(172, 688);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(233, 17);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Нет зашифрованного текста";
+            this.label7.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(21, 708);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1130, 109);
+            this.label6.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(172, 511);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 17);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Не введен текст или ключ!";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(21, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 16);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Введите ваш текст:";
+            // 
+            // itog
+            // 
+            this.itog.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.itog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.itog.ForeColor = System.Drawing.Color.Lime;
+            this.itog.Location = new System.Drawing.Point(21, 531);
+            this.itog.Name = "itog";
+            this.itog.Size = new System.Drawing.Size(1130, 109);
+            this.itog.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(21, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(314, 17);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Вводите только на выбранном языке!";
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(18, 452);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Ошибка ввода";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(18, 398);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Ключ:";
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.title.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.ForeColor = System.Drawing.Color.Lime;
+            this.title.Location = new System.Drawing.Point(21, 15);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(1130, 26);
+            this.title.TabIndex = 23;
+            this.title.Text = "Приветствуем! Программа посвящена шифру Цезаря. Выберите язык, введите текст, цел" +
+    "очисленный ключ и выберите нужную опцию.\r\n";
+            // 
+            // eng
+            // 
+            this.eng.AutoSize = true;
+            this.eng.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.eng.ForeColor = System.Drawing.Color.Lime;
+            this.eng.Location = new System.Drawing.Point(123, 70);
+            this.eng.Name = "eng";
+            this.eng.Size = new System.Drawing.Size(120, 21);
+            this.eng.TabIndex = 22;
+            this.eng.Text = "английский";
+            this.eng.UseVisualStyleBackColor = true;
+            this.eng.CheckedChanged += new System.EventHandler(this.eng_CheckedChanged);
+            // 
+            // rus
+            // 
+            this.rus.AutoSize = true;
+            this.rus.Checked = true;
+            this.rus.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rus.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rus.ForeColor = System.Drawing.Color.Lime;
+            this.rus.Location = new System.Drawing.Point(24, 70);
+            this.rus.Name = "rus";
+            this.rus.Size = new System.Drawing.Size(93, 21);
+            this.rus.TabIndex = 21;
+            this.rus.Text = "русский";
+            this.rus.UseVisualStyleBackColor = true;
+            this.rus.CheckedChanged += new System.EventHandler(this.rus_CheckedChanged);
+            // 
+            // decrypt
+            // 
+            this.decrypt.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.decrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.decrypt.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decrypt.ForeColor = System.Drawing.Color.Lime;
+            this.decrypt.Location = new System.Drawing.Point(21, 666);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(145, 39);
+            this.decrypt.TabIndex = 20;
+            this.decrypt.Text = "Расшифровать";
+            this.decrypt.UseVisualStyleBackColor = false;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // crypt
+            // 
+            this.crypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.crypt.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.crypt.ForeColor = System.Drawing.Color.Lime;
+            this.crypt.Location = new System.Drawing.Point(21, 489);
+            this.crypt.Name = "crypt";
+            this.crypt.Size = new System.Drawing.Size(145, 39);
+            this.crypt.TabIndex = 19;
+            this.crypt.Text = "Зашифровать";
+            this.crypt.UseVisualStyleBackColor = true;
+            this.crypt.Click += new System.EventHandler(this.crypt_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.ForeColor = System.Drawing.Color.Lime;
+            this.textBox3.Location = new System.Drawing.Point(21, 418);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1130, 22);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(24, 121);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1127, 233);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1168, 832);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.itog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.eng);
+            this.Controls.Add(this.rus);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.crypt);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label itog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.CheckBox eng;
+        private System.Windows.Forms.CheckBox rus;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.Button crypt;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Лаба_3/Лаба_3/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба_3
+{
+    public partial class Form1 : Form
+    {
+        //специальные строки для проверки вводимого текста
+        string eng_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890\n\r ";
+        string rus_string = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890\n\r ";
+        int key;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Viszener_Program";
+        }
+        static string ViszCrypt(string text, string key)
+        {
+            string alph_eng = "abcdefghijklmnopqrstuvwxyz";
+            string alph_rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            string numb = "1234567890";
+            string newKey = key;
+            bool encode = true;
+            int crypt_sign = encode ? +1 : -1; //знак для шифровки или расшифровки
+            int offset;
+            int indexOf;
+            string result = "";
+            while (newKey.Length < text.Length)
+            {
+                newKey += key;
+            }
+            if (newKey.Length > text.Length)
+            {
+                newKey = newKey.Substring(0, text.Length);
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (alph_rus.Contains(Char.ToLower(text[i])))
+                {
+                    indexOf = alph_rus.IndexOf(text[i]);
+                    if (indexOf != -1)
+                    {
+                        offset = alph_rus.IndexOf(text[i]) + alph_rus.IndexOf(newKey[i]) * crypt_sign;
+                        if (offset >= alph_rus.Length)
+                            offset = offset - alph_rus.Length;
+                        else if (offset < 0)
+                            offset = alph_rus.Length + offset;
+                        result += alph_rus[offset];
+                    }
+                    else
+                        result += text[i];
+                }
+                else if (alph_eng.Contains(Char.ToLower(text[i])))
+                {
+                    indexOf = alph_eng.IndexOf(text[i]);
+                    if (indexOf != -1)
+                    {
+                        offset = alph_eng.IndexOf(text[i]) + alph_eng.IndexOf(newKey[i]) * crypt_sign;
+                        if (offset >= alph_eng.Length)
+                            offset = offset - alph_eng.Length;
+                        else if (offset < 0)
+                            offset = alph_eng.Length + offset;
+                        result += alph_eng[offset];
+                    }
+                    else
+                        result += text[i];
+                }
+                else if (numb.Contains(text[i]))
+                {
+                    result += text[i];
+                }
+                else if (text[i] == ' ')
+                {
+                    result += text[i];
+                }
+                
+            }
+            return result;
+        }
+        private void rus_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rus.Checked == true) //проверка свойства (выбрана или нет)
+            {
+                eng.Checked = false; //меняем свойство другой кнопки 
+                label3.Visible = false; //прячем текст об ошибке
+                textBox1.Text = ""; //очистка поля
+            }
+            else
+            {
+                eng.Checked = true;
+            }
+        }
+
+        private void eng_CheckedChanged(object sender, EventArgs e)
+        {
+            if (eng.Checked == true)
+            {
+                rus.Checked = false; //меняем свойство другой кнопки 
+                label3.Visible = false; //прячем текст об ошибке
+                textBox1.Text = ""; //очистка поля
+            }
+            else
+            {
+                rus.Checked = true;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string eng_up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string rus_up = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            textBox1.ScrollBars = ScrollBars.Vertical; //добавление вертикального скроллбара если текст больше поля
+            string text = textBox1.Text; //вытаскиваем текст для проверки
+            //проверка вводимого текста
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (rus.Checked == true && !rus_string.Contains(text[i]))
+                //не даем ввести все что не входит в контрольные строки
+                {
+                    label3.Visible = true;
+                    textBox1.Text = text.Remove(text.Length - 1);
+                }
+                if (eng.Checked == true && !eng_string.Contains(text[i]))
+                {
+                    label3.Visible = true;
+                    textBox1.Text = text.Remove(text.Length - 1);
+                }
+
+            }
+            
+            //переводим каретку в конец (точку старта ввода)
+            textBox1.SelectionStart = textBox1.Text.Length;
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string eng_string1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+            string rus_string1 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    
+            string text = textBox3.Text; //извлекаем текст в поле ключа
+            for (int i = 0; i < textBox3.Text.Length; i++)
+            {
+                if (rus.Checked == true && !rus_string1.Contains(text[i]))
+                //не даем ввести все что не входит в контрольные строки
+                {
+                    label2.Visible = true;
+                    textBox3.Text = text.Remove(text.IndexOf(text[i]));
+                }
+                if (eng.Checked == true && !eng_string1.Contains(text[i]))
+                {
+                    label2.Visible = true;
+                    textBox3.Text = text.Remove(text.IndexOf(text[i]));
+                }
+            }
+            
+            //если совершены ошибки возвращаем каретку в конец строки
+            textBox3.SelectionStart = textBox3.Text.Length;
+            //присваиваем введеное число в ключ?
+            if (textBox3.Text != "" && textBox3.Text != "-")
+            {
+                string key_string = textBox3.Text;
+            }
+        }
+
+        private void crypt_Click(object sender, EventArgs e)
+        {
+            string str = "1234567890-";
+            bool flag = false;
+            string key = textBox3.Text;
+            for (int i = 0; i < textBox3.Text.Length; i++)
+            {
+                if (str.Contains(key[i]))
+                {
+                    flag = true;
+                }
+            }
+            if (textBox3.Text != "" && flag != true && textBox1.Text != "")
+            {
+                string crypto_text;
+                crypto_text = ViszCrypt(textBox1.Text, textBox3.Text);
+                itog.Text = crypto_text;
+            }
+            else
+            {
+                label5.Visible = true;
+            }
+            flag = false;
+        }
+
+        static string ViszDecrypt(string text, string key)
+        {
+            string alph_eng = "abcdefghijklmnopqrstuvwxyz";
+            string alph_rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            string newKey = key;
+            bool encode = false;
+            int crypt_sign = encode ? +1 : -1; //знак для шифровки или расшифровки
+            int offset;
+            int indexOf;
+            string result = "";
+            while (newKey.Length < text.Length)
+            {
+                newKey += key;
+            }
+            if (newKey.Length > text.Length)
+            {
+                newKey = newKey.Substring(0, text.Length);
+            }
+            newKey = newKey.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (alph_rus.Contains(Char.ToLower(text[i])))
+                {
+                    indexOf = alph_rus.IndexOf(text[i]);
+                    if (indexOf != -1)
+                    {
+                        offset = alph_rus.IndexOf(text[i]) + alph_rus.IndexOf(newKey[i]) * crypt_sign;
+                        if (offset >= alph_rus.Length)
+                            offset = offset - alph_rus.Length;
+                        else if (offset < 0)
+                            offset = alph_rus.Length + offset;
+                        result += alph_rus[offset];
+                    }
+                    else
+                        result += text[i];
+                }
+                else
+                {
+                    indexOf = alph_eng.IndexOf(text[i]);
+                    if (indexOf != -1)
+                    {
+                        offset = alph_eng.IndexOf(text[i]) + alph_eng.IndexOf(newKey[i]) * crypt_sign;
+                        if (offset >= alph_eng.Length)
+                            offset = offset - alph_eng.Length;
+                        else if (offset < 0)
+                            offset = alph_eng.Length + offset;
+                        result += alph_eng[offset];
+                    }
+                    else
+                        result += text[i];
+                }
+            }
+            return result;
+        }
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            string str = "1234567890-";
+            bool flag = false;
+            string key = textBox3.Text;
+            for (int i = 0; i < textBox3.Text.Length; i++)
+            {
+                if (str.Contains(key[i]))
+                {
+                    flag = true;
+                }
+            }
+            if (textBox3.Text != "" && flag != true && itog.Text != "")
+            {
+                string crypto_text;
+                crypto_text = ViszDecrypt(itog.Text, textBox3.Text);
+                label6.Text = crypto_text;
+            }
+            else
+            {
+                label7.Visible = true;
+            }
+            flag = false;
+        }
+    }
+}
